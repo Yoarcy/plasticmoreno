c2d2e17172f89580e2a086f19102ffb94e2edb3c
Yoarcy Echazabal <yoarcy82@gmail.com>
2022-01-28 04:05:49 -0500

    agregé el lightbox todo ok

--- slider.js
+++ slider.js
@@ -6,6 +6,11 @@ let sliderSelectionLast = sliderSelection[sliderSelection.length -1];
 const btnLeft = document.querySelector('#btn-left');
 const btnRight = document.querySelector('#btn-right');
 
+const enlaces = document.querySelectorAll('.ul .a')
+const lightbox =document.querySelector('.lightbox')
+const grande = document.querySelector('.grande')
+const cerrar = document.querySelector('.cerrar')
+
 slider.insertAdjacentElement('afterbegin', sliderSelectionLast);
 
 function next() {
@@ -41,3 +46,18 @@ btnLeft.addEventListener('click', function () {
 setInterval(() => {
    next(); 
 },4000);
+
+enlaces.forEach((cadaEnlace , i)=>{
+    enlaces[i].addEventListener('click', (e)=>{
+        e.preventDefault()
+        let ruta = cadaEnlace.querySelector('.img').src
+        console.log (ruta)
+        lightbox.classList.add('activo')
+        grande.setAttribute('src', ruta)
+    })
+})
+
+cerrar.addEventListener('click',()=>{
+    lightbox.classList.remove('activo')
+    // grande.setAttribute('src', '')
+})
